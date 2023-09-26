221fad91f963938f5601ff6fb5845d5affb06f16
benhord <benjhord@gmail.com>
2023-09-26 14:28:15 -0400

    Fixing some tests and removing functions

--- tests/test_pandora_sat.py
+++ tests/test_pandora_sat.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 
 # First-party/Local
-from pandorasat import PACKAGEDIR, PandoraSat, Target, __version__
+from pandorasat import PACKAGEDIR, PandoraSat, __version__
 
 
 def test_version():
@@ -20,19 +20,4 @@ def test_pandorasat():
     visda.sensitivity(wavelength)
     assert np.isclose(nirda.midpoint.value, 1.29750, atol=1e-3)
     assert np.isclose(visda.midpoint.value, 0.55399, atol=1e-3)
-    return
-
-
-@pytest.mark.remote_data
-def test_trace():
-    p = PandoraSat(ra=180 * u.deg, dec=0 * u.deg, theta=10 * u.deg)
-    t = Target.from_gaia("GJ 436")
-    nirda = p.NIRDA
-    wavelength = np.linspace(0.1, 2, 6000) * u.micron
-    spectrum = t.spectrum(wavelength)
-    nirda.get_trace(
-        wavelength,
-        spectrum.value**0 * spectrum.unit,
-        target_center=[40, 250],
-    )
     return
