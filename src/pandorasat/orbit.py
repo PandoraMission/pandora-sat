2861f39332bbefb4c717e4999511d17b8980016d
benhord <benjhord@gmail.com>
2023-10-10 13:34:56 -0400

    Added planning notes

--- src/pandorasat/orbit.py
+++ src/pandorasat/orbit.py
@@ -22,3 +22,9 @@ class Orbit:
 
     def __repr__(self):
         return "Pandora Orbit"
+
+#   pre-launch: generate dummy orbit based on expected orbital params
+#   methods to pull stats from orbit (e.g. period, velocity at points, RA/Dec, altitude, etc)
+
+#   upon launch switch to JPL Horizons query for updates
+#   still generate orbit but periodically updated with JPL Horizons input (every X period of time)
