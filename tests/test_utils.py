2ed78aa65dd7a17505d46579f03caae25a4031f0
benhord <benjhord@gmail.com>
2023-10-10 13:31:06 -0400

    Added tests for each utils function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,41 @@
-# test photon energy here
+# Standard library
+import os
+from glob import glob
 
-# test get_jitter here
+# Third-party
+import astropy.units as u
+import numpy as np
 
-# test get-flatfield here
+# First-party/Local
+from pandorasat import utils, PACKAGEDIR
+
+
+# test photon_energy
+def test_photon_energy():
+    assert np.isclose(utils.photon_energy(0.21 * u.m).value, 9.41e-25, atol=1e-25)
+    return
+
+
+# test get_flatfield
+def test_get_flatfield():
+    utils.get_flatfield()
+
+    # Check that files were made for NIRDA and VISDA
+    assert len(glob(f"{PACKAGEDIR}/data/flatfield_NIRDA*.fits")) > 0
+    assert len(glob(f"{PACKAGEDIR}/data/flatfield_VISDA*.fits"))
+    return
+
+
+# test load_vega
+def test_load_vega():
+    # Check that the file exists
+    assert os.path.isfile(f"{PACKAGEDIR}/data/vega.dat")
+
+    wav, spec = utils.load_vega()
+
+    # Check that the loaded file is correct
+    assert isinstance(wav, u.Quantity)
+    assert isinstance(spec, u.Quantity)
+    assert len(wav) > 0
+    assert len(spec) > 0
+    return
