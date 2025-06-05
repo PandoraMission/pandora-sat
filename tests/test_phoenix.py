be8cd0e8f84ae6340f5c5805c57e69c856ba3bcc
Yoav Rotman <rotman3@dane4.llnl.gov>
2025-06-05 15:45:13 -0700

    Updated tests for phoenix.py

--- tests/test_phoenix.py
+++ tests/test_phoenix.py
@@ -1,22 +1,24 @@
 # Standard library
 import os
-from glob import glob
-
-# Third-party
-import astropy.units as u
-import numpy as np
-import stsynphot as stsyn
-import pytest
-
 
 # First-party/Local
-from pandorasat import PACKAGEDIR, phoenix
+from pandorasat import phoenix
+
+PHOENIXPATH = phoenix.PHOENIXPATH
+os.environ["PYSYN_CDBS"] = PHOENIXPATH
+
+# Third-party
+import astropy.units as u  # noqa: E402
+import numpy as np  # noqa: E402
+import pytest  # noqa: E402
+import stsynphot as stsyn  # noqa: E402
+
 
 # testing get_vega
 def test_get_vega():
-    PHOENIXPATH = phoenix.PHOENIXPATH
     phoenix.get_vega()
-    assert os.path.isfile(PHOENIXPATH+'/calspec/alpha_lyr_stis_011.fits')
+    assert os.path.isfile(PHOENIXPATH + "/calspec/alpha_lyr_stis_011.fits")
+
 
 # test get_phoenix_model
 def test_spectrum_generation():
@@ -26,7 +28,9 @@ def test_spectrum_generation():
         )
     try:
         wavelength, sed = phoenix.get_phoenix_model(teff=5000, jmag=9)
-    except: pytest.fail("Failed to generate a spectrum")
+    except Exception:
+        pytest.fail("Failed to generate a spectrum")
+
 
 # test get_phoenix_model units
 def test_output_units():
@@ -35,7 +39,6 @@ def test_output_units():
             "Skipping this test on GitHub Actions this downloads a database of stellar models."
         )
     wavelength, sed = phoenix.get_phoenix_model(teff=5000, jmag=9)
-    assert len(wavelength) == len(sed)
     try:
         u.Quantity(wavelength, u.AA)
     except u.UnitConversionError:
@@ -45,17 +48,44 @@ def test_output_units():
     except u.UnitConversionError:
         pytest.fail("Incorrect units")
 
+
+# test get_phoenix_model outputs in correct wavelength range and all SED values positive
+def test_output_inrange():
+    if os.getenv("GITHUB_ACTIONS") == "true":
+        pytest.skip(
+            "Skipping this test on GitHub Actions this downloads a database of stellar models."
+        )
+    wavelength, sed = phoenix.get_phoenix_model(teff=5000, jmag=9)
+    assert np.all(sed.value >= 0)
+    assert np.all((wavelength.value >= 1000) & (wavelength.value <= 30000))
+
+
+# test get_phoenix_model output length
+def test_output_length():
+    if os.getenv("GITHUB_ACTIONS") == "true":
+        pytest.skip(
+            "Skipping this test on GitHub Actions this downloads a database of stellar models."
+        )
+    wavelength, sed = phoenix.get_phoenix_model(teff=5000, jmag=9)
+    assert len(wavelength) == len(sed)
+
+
 # test get_phoenix_model errors out of grid range
 def test_out_of_grid_T():
     if os.getenv("GITHUB_ACTIONS") == "true":
         pytest.skip(
             "Skipping this test on GitHub Actions this downloads a database of stellar models."
         )
-    with pytest.raises(UnboundLocalError): wavelength, sed = phoenix.get_phoenix_model(teff=10000, jmag=9)
+    with pytest.raises(UnboundLocalError):
+        wavelength, sed = phoenix.get_phoenix_model(teff=10000, jmag=9)
+
 
 def test_out_of_grid_logg():
     if os.getenv("GITHUB_ACTIONS") == "true":
         pytest.skip(
             "Skipping this test on GitHub Actions this downloads a database of stellar models."
         )
-    with pytest.raises(stsyn.exceptions.ParameterOutOfBounds): wavelength, sed = phoenix.get_phoenix_model(teff=5000, logg=15., jmag=9)
+    with pytest.raises(stsyn.exceptions.ParameterOutOfBounds):
+        wavelength, sed = phoenix.get_phoenix_model(
+            teff=5000, logg=15.0, jmag=9
+        )
