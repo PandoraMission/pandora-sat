771668c0fe52272f6e73556903e8de3a615ad819
Christina Hedges <christina.l.hedges@nasa.gov>
2025-09-24 13:51:30 -0400

    add vega test to github ci

--- tests/test_phoenix.py
+++ tests/test_phoenix.py
@@ -16,13 +16,7 @@ import stsynphot as stsyn  # noqa: E402
 
 # testing get_vega
 def test_get_vega():
-    if os.getenv("GITHUB_ACTIONS") == "true":
-        pytest.skip(
-            "Skipping this test on GitHub Actions this downloads a database of stellar models."
-        )
-
-    phoenix.download_vega()
-    assert os.path.isfile(PHOENIXPATH + "/calspec/alpha_lyr_stis_011.fits")
+    assert os.path.isfile(PHOENIXPATH + "calspec/alpha_lyr_stis_011.fits")
 
     wav, spec = phoenix.load_vega()
 
